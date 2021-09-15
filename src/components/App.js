a1d1128c1f88b38a2cba0d165805870beac41601
Jeremiah <jsnidar@gmail.com>
2021-09-15 07:34:34 -0500

    commit tree 6efff07261bb9c13758710339e1961814fe466c4

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
 
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(response => response.json())
+    .then(data => setQuestions(data)
+    )}, [])
+  
+  function addQuestion(newQuestion) {
+    const updatedQuestions = [...questions, newQuestion]
+    setQuestions(updatedQuestions)
+  }
+
+  function handleDelete (id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(() => {
+      const updatedQuestions = questions.filter((question) => question.id !== id)
+      setQuestions(updatedQuestions)
+    });
+  }
+
+  function updateCorrectAnswer(id, index){
+    const int = parseInt(index, 10)
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        correctIndex: index
+      })
+    })
+  }
+  
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm questions={questions} addQuestion={addQuestion}/> : <QuestionList questions={questions} handleDelete={handleDelete} updateCorrectAnswer={updateCorrectAnswer} />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,15 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList( {questions, handleDelete, updateCorrectAnswer } ) {
+  console.log(questions)
+  
+  const listOfQuestions = questions.map((question) => <QuestionItem key={question.id} question={question} handleDelete={handleDelete} updateCorrectAnswer={updateCorrectAnswer} />);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{listOfQuestions}</ul>
     </section>
   );
 }
